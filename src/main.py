f8a034dfed08d784a828018e21b7acf832d58dad
R00tDev1l <74395487+Darkhaxxor005@users.noreply.github.com>
2026-01-12 22:14:31 +0600

    Math Function Implementation
    Added Jacobi and Initial grid.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt #Libraries for math calculation and graph plotting.
+import matplotlib.pyplot as plt    # Libraries for math calculation and graph plotting.
+from solvers import jacobi,apply_boundary_conditions  # Math Functions
 
-
-#User Input Section
+# User Input Section
 
 print("\n2D Laplace Equation Solver using Iterative Methods\n")
 N = int(input("\n[-]Enter grid size N (e.g. 50): "))
@@ -10,5 +10,8 @@ max_iter = int(input("\n[-]Enter maximum iterations (e.g. 5000): "))
 tol = float(input("\n[-]Enter tolerance (e.g. 1e-6): "))
 omega = float(input("\n[-]Enter SOR relaxation factor omega (e.g. 1.7): "))
 
+# Initial Grid
 
+u0 = np.zeros((N, N))
+u0 = apply_boundary_conditions(u0)
 
