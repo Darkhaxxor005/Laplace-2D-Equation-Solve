a3cee90d47d82679571211cf98a1ec71829ae6c6
R00tDev1l <74395487+Darkhaxxor005@users.noreply.github.com>
2026-01-13 19:14:59 +0600

    SOR Algorithm
    This function uses finite difference discretization equation by using SOR algorithm. It returns answer and error.

--- src/solvers.py
+++ src/solvers.py
@@ -63,3 +63,29 @@ def gauss_seidel(u, max_iter, tol):
         u = apply_boundary_conditions(u)
 
     return u, errors  # Returns u and errors to the main function.                
+
+def sor(u, omega, max_iter, tol):
+
+    # Solve 2D Laplace equation using the SOR method.
+
+    errors = []
+
+    for _ in range(max_iter):
+        u_old = u.copy()
+
+        for i in range(1, u.shape[0] - 1):
+            for j in range(1, u.shape[1] - 1):
+                u[i, j] = (1 - omega) * u[i, j] + omega * 0.25 * (
+                    u[i + 1, j] + u[i - 1, j] +       # Finite Difference Discretization Equation
+                    u[i, j + 1] + u[i, j - 1]
+                )
+
+        error = np.linalg.norm(u - u_old)     # The error is defined as the L2 norm of the difference between successive solution iterates.
+        errors.append(error)
+
+        if error < tol:
+            break   # Breaks the loop when the error is smaller than the error threshold.
+
+        u = apply_boundary_conditions(u)
+
+    return u, errors  # Returns u and errors to the main function.
